57ca12271af19bdc17c85c0b5558422129c12a71
RoyPeng1 <3186357040@qq.com>
2020-03-20 14:49:25 +0800

    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx

--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -2,5 +2,6 @@ public class TestMain {
     public static void main(String[] args){
         System.out.println("北网 北网 北网");
         System.out.println("bj bj bj bj bj");
+        System.out.println("明天会更好");
     }
 }
